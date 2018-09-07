9189e5ed738e7c687c5a493fb4e3a4516afe7683
catsigma <im@catsigma.com>
2018-09-07 15:19:08 +0800

    add prompt info and update README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # tradebot
 trade bot lib for tez.exchange
+
+## How to install
+```
+npm install --save tezexchange-tradebot
+```
+
+## How to use
+```
+const tradebot = require('tezexchange-tradebot')
+
+tradebot.getApiClient()
+.then(ac => {
+  return ac.getTokenInfo(ac.tokens.TES, ac.client.key_pair.public_key_hash)
+})
+.then(x => console.log(x))
+```
--- src/index.js
+++ src/index.js
@@ -1,31 +1,40 @@
 import { TZClient } from './tzclient.js'
 import Api from 'tezexchange-api'
 import readline from 'readline'
+import Stream from 'stream'
 
-const readInput = (q) => {
+const readInput = (q, need_hide) => {
   return new Promise((resolve, reject) => {
     const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout
     })
-    rl._writeToOutput = function _writeToOutput(stringToWrite) {
-      if (stringToWrite.length === 1)
-        rl.output.write("*")
-      else
-        rl.output.write(stringToWrite)
-    }
-    rl.question(q, (answer) => {
+
+    console.log(q)
+    rl.question('', (answer) => {
       rl.close()
       resolve(answer)
     })
+
+    if (need_hide)
+      rl._writeToOutput = function _writeToOutput(stringToWrite) {
+        rl.output.write('')
+      }
   })
 }
 
 export const getApiClient = async (version) => {
   const tzclient = new TZClient()
   await tzclient.ready
-  const encrypted_seed = await readInput('Encrypted Seed(edesk):')
-  const password = await readInput('Password:')
-  await tzclient.importKey({encrypted_seed, password})
+  const choice = await readInput('Using unencrypted key(1) or encrypted seed(2):')
+  if (choice == 1) {
+    const secret_key = await readInput('Unencrypted key(edsk):', true)
+    await tzclient.importKey({secret_key})
+  } else if (choice == 2) {
+    const encrypted_seed = await readInput('Encrypted Seed(edesk):', true)
+    const password = await readInput('Password:', true)
+    await tzclient.importKey({encrypted_seed, password})
+  }
+  console.log('=== KEY IMPORTED ===')
   return new Api(tzclient, null, version)
 }
