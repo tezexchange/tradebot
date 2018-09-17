6ac0e084fc5c10b8e6bf3adffac59c14b8d616db
catsigma <im@catsigma.com>
2018-09-17 11:02:31 +0800

    add hardcoded key import method

--- src/index.js
+++ src/index.js
@@ -23,18 +23,31 @@ const readInput = (q, need_hide) => {
   })
 }
 
-export const getApiClient = async (version) => {
+export const getApiClient = async ({
+  version,
+  secret_key,
+  encrypted_seed
+}) => {
   const tzclient = new TZClient()
   await tzclient.ready
-  const choice = await readInput('Using unencrypted key(1) or encrypted seed(2):')
-  if (choice == 1) {
-    const secret_key = await readInput('Unencrypted key(edsk):', true)
+
+  if (secret_key) {
     await tzclient.importKey({secret_key})
-  } else if (choice == 2) {
-    const encrypted_seed = await readInput('Encrypted Seed(edesk):', true)
+  } else if (encrypted_seed) {
     const password = await readInput('Password:', true)
     await tzclient.importKey({encrypted_seed, password})
+  } else {
+    const choice = await readInput('Using unencrypted key(1) or encrypted seed(2):')
+    if (choice == 1) {
+      const secret_key = await readInput('Unencrypted key(edsk):', true)
+      await tzclient.importKey({secret_key})
+    } else if (choice == 2) {
+      const encrypted_seed = await readInput('Encrypted Seed(edesk):', true)
+      const password = await readInput('Password:', true)
+      await tzclient.importKey({encrypted_seed, password})
+    }
   }
+
   console.log('=== KEY IMPORTED ===')
   return new Api(tzclient, null, version)
 }
